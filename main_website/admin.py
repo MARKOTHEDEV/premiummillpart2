772acde16bed991b3fff9a94d35a50009edbc6a8
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-07-26 01:15:32 -0700

    updated the DJango admin site Rename all the Headers to Premium Milll

--- main_website/admin.py
+++ main_website/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from main_website.models import (User,UserPayment,Enquiry,PhoneNumber,BitcoinAddresse,User_Editable_Balance)
 
+admin.site.site_header = "Premium Mill Admin"
+admin.site.site_title = "Premium Mill Admin Portal"
+admin.site.index_title = "Welcome to Premium MillPortal"
+
 admin.site.register(User)
 admin.site.register(UserPayment)
 admin.site.register(User_Editable_Balance)
