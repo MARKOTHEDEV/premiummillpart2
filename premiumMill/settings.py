3ed5589816a68d0947dd39fd13e285d23d5af6fc
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-07-31 03:36:33 -0700

    added more allowed host

--- premiumMill/settings.py
+++ premiumMill/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = 'c$%w$$4^d%2k592ph5jjpxhw93$y-03h!+w*xin(c(25fwo^y8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['marko-premiummill.herokuapp.com']
+ALLOWED_HOSTS = ['marko-premiummill.herokuapp.com','www.premiumill.com','premiumill.com']
 
 
 # Application definition
